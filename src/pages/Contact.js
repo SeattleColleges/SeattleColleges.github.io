71defbc810a9f0ef2dfa4cb56c61195eb143df7f
Alzavio <luiz.matthew187@gmail.com>
2024-05-22 21:01:23 -0700

    Created the structure for the contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,8 +1,24 @@
+import Navbar from "../components/Navbar";
+
 function Contact() {
     return (
-      <div>
-        <p>Placeholder Contact Page</p>
-      </div>
+      <>
+          <Navbar />
+          <div style={{minHeight: "calc(100vh - 95px)", marginTop: "2rem", marginLeft: "2rem", marginRight: "2rem", height: "1px"}}>
+              <div style={{
+                  display: "flex",
+                  flexWrap: "wrap",
+                  height: "100%"
+              }}>
+                  <div style={{flexGrow: 1, "-webkit-box-flex": 1, flexBasis: 0, maxWidth: "100%"}}>
+                      test
+                  </div>
+                  <div style={{flexGrow: 1, "-webkit-box-flex": 1, flexBasis: 0, maxWidth: "100%"}}>
+                      test
+                  </div>
+              </div>
+          </div>
+      </>
     );
   }
   
