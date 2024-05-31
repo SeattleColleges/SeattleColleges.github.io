83af872036a3e40e61eed0b66c0d0fb09f753708
Bradley Charles <bradgcharles@gmail.com>
2024-05-30 17:00:21 -0700

    finished about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,6 @@
 import Navbar from "../components/Navbar";
 import { Helmet } from "react-helmet";
+import NSC_Marquee from "../images/about/NSC_Marquee.jpg";
 
 function About() {
   return (
@@ -8,54 +9,93 @@ function About() {
         <title> NSC App Development - About </title>
       </Helmet>
       <Navbar />
-      <div style={{ minHeight: "calc(100vh - 95px)", height: "1px" }}>
+
+      <div
+        style={{
+          marginTop: "10vh",
+          marginBottom: "10vh",
+          height: "100%",
+        }}
+      >
         <div
           style={{
-            marginTop: "10vh",
-            marginBottom: "10vh",
-            marginLeft: "2rem",
-            marginRight: "2rem",
-            height: "100%",
+            ...flexParent,
+            height: "auto",
           }}
         >
           <div
-            style={{ fontSize: "48px", marginBottom: "2rem", fontWeight: 600 }}
+            style={{
+              ...flexChild,
+              display: "flex",
+              justifyContent: "center",
+              paddingTop: "30px",
+              width: "30%",
+            }}
           >
-            North Seattle Application Development
-          </div>
-          <div>
-            <strong>Established in</strong> imperdiet dui accumsan sit amet. Non
-            pulvinar neque laoreet suspendisse interdum consectetur libero id
-            faucibus. Vehicula ipsum a arcu cursus vitae congue mauris rhoncus.
-            In nibh mauris cursus mattis molestie a. Dignissim convallis aenean
-            et tortor at risus viverra.
-          </div>
-
-          <div>
-            <strong>Tellus in hac habitasse</strong> platea dictumst.
-            Ullamcorper eget nulla facilisi etiam dignissim. Mauris ultrices
-            eros in cursus turpis massa. Cras adipiscing enim eu turpis egestas
-            pretium aenean pharetra magna. Est placerat in egestas erat
-            imperdiet sed euismod. Faucibus vitae aliquet nec ullamcorper.
+            <div
+              style={{
+                width: "70%",
+                fontSize: "56px",
+                fontWeight: 600,
+              }}
+            >
+              North Seattle Application Development
+            </div>
           </div>
           <div
             style={{
-              ...flexParent,
-              height: "100%",
-              backgroundColor: "blue",
+              ...flexChild,
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              width: "70%",
             }}
           >
-            <div style={flexChild} className={"col-sm-6"}></div>
             <div
               style={{
                 ...flexChild,
-                display: "flex",
-                justifyContent: "center",
+                fontSize: "24px",
+                width: "80%",
               }}
-              className={"col-sm-6"}
-            ></div>
+            >
+              <div style={{ marginBottom: "1rem" }}>
+                <strong>Established in</strong> imperdiet dui accumsan sit amet.
+                Non pulvinar neque laoreet suspendisse interdum consectetur
+                libero id faucibus. Vehicula ipsum a arcu cursus vitae congue
+                mauris rhoncus. In nibh mauris cursus mattis molestie a.
+                Dignissim convallis aenean et tortor at risus viverra.
+              </div>
+
+              <div>
+                <strong>Tellus in hac habitasse</strong> platea dictumst.
+                Ullamcorper eget nulla facilisi etiam dignissim. Mauris ultrices
+                eros in cursus turpis massa. Cras adipiscing enim eu turpis
+                egestas pretium aenean pharetra magna. Est placerat in egestas
+                erat imperdiet sed euismod. Faucibus vitae aliquet nec
+                ullamcorper.
+              </div>
+            </div>
           </div>
         </div>
+      </div>
+      <img
+        src={NSC_Marquee}
+        alt="North Seattle College"
+        style={{
+          display: "block",
+        }}
+      />
+      <div
+        style={{
+          backgroundColor: "#165db2",
+          height: "190px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          color: "gray",
+        }}
+      >
+        Placeholder Footer
       </div>
     </>
   );
@@ -63,7 +103,6 @@ function About() {
 
 const flexChild = {
   position: "relative",
-  width: "100%",
   minHeight: "1px",
   paddingRight: "15px",
   paddingLeft: "15px",
