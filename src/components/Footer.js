a99cee604d95c23536548419bf065f708f1283a2
BradleyCharles <bradgcharles@gmail.com>
2024-06-03 14:19:40 -0700

    added text to test spacing

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,9 +9,10 @@ const Footer = () => {
         display: "flex",
         flexDirection: "row",
         height: "180px",
-        gap: "1rem",
+        gap: "20rem",
         backgroundColor: "#165db2",
         alignItems: "center",
+        justifyContent: "space-around",
       }}
     >
       <Link to={"/"}>
@@ -26,8 +27,47 @@ const Footer = () => {
           }}
         />
       </Link>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          gap: "5rem",
+          backgroundColor: "white",
+        }}
+      >
+        <ul style={{ ...footerList }}>
+          <div style={{ ...footerCategory }}>Navigation</div>
+          <li>Contact</li>
+          <li>About</li>
+          <li>Students</li>
+          <li>Portfolio</li>
+        </ul>
+        <ul style={{ ...footerList }}>
+          <div style={{ ...footerCategory }}>category</div>
+          <li>List item</li>
+        </ul>
+        <ul style={{ ...footerList }}>
+          <div style={{ ...footerCategory }}>category</div>
+          <li>List item</li>
+        </ul>
+      </div>
+      <div
+        style={{
+          backgroundColor: "green",
+        }}
+      >
+        Text
+      </div>
     </div>
   );
+};
+
+const footerCategory = {
+  fontWeight: 600,
+};
+
+const footerList = {
+  listStyleType: "none",
 };
 
 export default Footer;
