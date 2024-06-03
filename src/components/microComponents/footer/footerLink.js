00c3f8a6b8384bad6a2faa333cbf47282259dc11
BradleyCharles <bradgcharles@gmail.com>
2024-06-03 15:29:39 -0700

    Created FooterLink micro-component

---
+++ src/components/microComponents/footer/footerLink.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+import { Link } from "react-router-dom";
+
+export function FooterLink(props) {
+  return (
+    <Link
+      style={{ color: "white", textDecoration: "none" }}
+      to={"/" + props.name}
+    >
+      {props.name}
+    </Link>
+  );
+}
