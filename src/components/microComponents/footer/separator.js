bdfc4b2db0baa90733ebb7e52c497d1d317d7335
BradleyCharles <bradgcharles@gmail.com>
2024-06-05 11:09:02 -0700

    created micro-component for spacing

---
+++ src/components/microComponents/footer/separator.js
@@ -0,0 +1,13 @@
+export function Separator() {
+  return (
+    <div
+      style={{
+        backgroundColor: "#666",
+        width: "100%",
+        height: ".1rem",
+        marginTop: ".2rem",
+        marginBottom: ".1rem",
+      }}
+    />
+  );
+}
